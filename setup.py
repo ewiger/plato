ab14319f59534cc3c0eacf42b1f11ab2c5d5647f
Yauhen Yakimovich <eugeny.yakimovitch@gmail.com>
2013-07-18 16:45:52 +0200

    modified:   setup.py - Minor changes related to PyPI

--- setup.py
+++ setup.py
@@ -2,14 +2,32 @@
 # TODO: migrate to distribute?
 #import distribute_setup
 #distribute_setup.use_setuptools()
+
+
+
+import os.path
+import sys
 from glob import glob
-#from distutils.core import setup
-from setuptools import setup
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+
+def readme():
+    try:
+        with open(os.path.join(os.path.dirname(__file__), 'README.md')) as f:
+            return f.read()
+    except (IOError, OSError):
+        return ''
+
 
 setup(
     name='PyPlato',
     version='0.1.2',
     description='Python Batching Utilities',
+    long_description=readme(),
     author='Yauhen Yakimovich',
     author_email='eugeny.yakimovitch@gmail.com',
     url='https://github.com/ewiger/plato',
